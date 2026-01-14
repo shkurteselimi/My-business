f5a5c305067986b2e69becdafdeb1acb916c7168
Shkurte Selimi <shkurtaselimi52@gmail.com>
2026-01-14 21:12:12 +0100

    third commit

--- js/shopping.js
+++ js/shopping.js
@@ -1,4 +1,5 @@
 function addProduct() {
+    const dropdown = document.querySelector(".dropdown");
     const table = document.getElementById("productTable");
     const row = table.insertRow();
 
@@ -9,3 +10,10 @@ function addProduct() {
         <td>In Stock</td>
     `;
 }
+// Toggle dropdown on mobile
+dropdown.addEventListener("click", (e) => {
+  if (window.innerWidth <= 768) {
+    e.preventDefault();
+    dropdown.classList.toggle("active");
+  }
+});
